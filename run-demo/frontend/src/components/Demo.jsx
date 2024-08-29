71e1a44c23a2adb7b44e1a1d854fd71d9118ff69
Lakshan Rathnayaka <lrlakshan@gmail.com>
2024-08-29 16:26:37 +0300

    WasmModule movement improvement and persistance positioning with page refresh

--- run-demo/frontend/src/components/Demo.jsx
+++ run-demo/frontend/src/components/Demo.jsx
@@ -12,6 +12,7 @@ import houseImage from "./../assets/house.png";
 import Freezer from "./visual_components/Freezer";
 import WashingMachine from "./visual_components/WashingMachine";
 import Orchestrator from "./../assets/orchestrator.png";
+import WebAssembly_Logo from "./../assets/WebAssembly_Logo.png";
 import { fetchData } from '../services/apiService';
 
 const PUBLIC_HOST = process.env.PUBLIC_HOST;
@@ -26,20 +27,14 @@ const Demo = () => {
   const logsQueueRef = useRef([]);
   const healthLogTimerRef = useRef(null);
 
-  const [codeToFreezerObjPos, setCodeToFreezerObjPos] = useState(({ x: 0, y: 0 }));
-  const [codeToWashingMachineObjPos, setCodeToWashingMachineObjPos] = useState(({ x: 0, y: 0 }));
-  const [isCodeMoveObjsVisible, setIsCodeMoveObjsVisible] = useState(false);
+  const [movingDeployments, setMovingDeployments] = useState([]);
+  const [activeDeployments, setActiveDeployments] = useState([]);
 
+  // Store reference for each devices if needs to get the device location in UI
   const deviceReferences = {
     "freezer": freezerRef,
     "washing-machine": washingMachineRef,
     // Add more device names and their references here
-  };
-
-  const deviceStateMap = {
-    "freezer": setCodeToFreezerObjPos,
-    "washing-machine": setCodeToWashingMachineObjPos,
-    // Add more device names and their corresponding state setters here
   };
 
   // Get the reference of the device
@@ -53,28 +48,42 @@ const Demo = () => {
       }
   }
 
-  // Update the device position in the state
-  const updateDevicePosition = (deviceName, position) => {
-    const setDevicePosition = deviceStateMap[deviceName];
-    if (setDevicePosition) {
-      setDevicePosition(position);
-    } else {
-      console.error(`No device fount to update postion: ${deviceName}`);
-    }
-  };
-
   // Move the code animation object to the device position
   const moveCodeAnimation = (deviceName) => {
     const deviceRef = getDeviceReference(deviceName);
     if (deviceRef.current) {
       const device = deviceRef.current.getBoundingClientRect();
+      const orchestrator = orchestratorRef.current.getBoundingClientRect();
       const newPosition = {
         x: device.left + device.width / 2,
         y: device.top + device.height / 2,
       };
-      updateDevicePosition(deviceName, newPosition);
+      const orchestratorPosition = {
+        x: orchestrator.left + orchestrator.width / 2,
+        y: orchestrator.top + orchestrator.height / 2,
+      };
+  
+      setMovingDeployments((prevDeployments) => {
+        const newMovingDeployments = [
+          ...prevDeployments,
+          {
+            id: prevDeployments.length, 
+            deviceName,
+            startPos: orchestratorPosition,
+            endPos: newPosition,
+          },
+        ];
+
+        setTimeout(() => {
+          setMovingDeployments((currentMovingDeployments) => 
+            currentMovingDeployments.filter(dep => dep.id !== newMovingDeployments[newMovingDeployments.length - 1].id)
+          );
+        }, 5000); // Remove after 5 seconds
+
+        return newMovingDeployments;  
+      });
     }
-  }
+  };
 
   // Reset the device storage after 3 minutes of inactivity
   const resetDeviceStorage = () => {
@@ -114,26 +123,56 @@ const Demo = () => {
   };
 
   // Update the deployment details for the device
-  const updateDeployment = async (device) => {
-    const deployments = await fetchData("/file/manifest");
+  const updateDeployment = async (device, deviceName, deployments) => {
  
-    const deviceSpecifiDeployment = deployments.find((item) =>
+    const deviceSpecificDeployment = deployments.find((item) =>
       item.sequence.some((seq) => seq.device === device.deviceId)
     );
  
-    if (deviceSpecifiDeployment) {
+    if (deviceSpecificDeployment) {
  
       // Accessing the modules inside fullManifest using the deviceId
-      const deviceManifest = deviceSpecifiDeployment.fullManifest[device.deviceId];
+      const deviceManifest = deviceSpecificDeployment.fullManifest[device.deviceId];
       
       device.existingModuleId = deviceManifest.modules[0].id;
       device.existingModuleName = deviceManifest.modules[0].name;
-      deviceSpecifiDeployment.active ? device.isModuleActive = true : device.isModuleActive = false;
+      deviceSpecificDeployment.active ? device.isModuleActive = true : device.isModuleActive = false;
+
+      const deviceRef = getDeviceReference(deviceName);
+
+      if (deviceRef.current && device.isModuleActive) {
+        const deviceBounds = deviceRef.current.getBoundingClientRect();
+
+        const wasmModuleIconPosition = {
+          x: deviceBounds.left + deviceBounds.width / 2,
+          y: deviceBounds.top + deviceBounds.height / 2,
+        };
+
+        setActiveDeployments((prevActiveDeployments) => {
+          const existingDeploymentIds = prevActiveDeployments.map((dep) => dep.id); 
+          
+          if (!existingDeploymentIds.includes(deviceSpecificDeployment._id)) {
+            return [
+              ...prevActiveDeployments,
+              {
+                id: deviceSpecificDeployment._id,
+                wasmModuleIconPosition: wasmModuleIconPosition,
+                deviceId: device.deviceId,
+              },
+            ];
+          }
+          return prevActiveDeployments;
+        });
+      }
 
     } else {
       device.existingModuleId = null;
       device.existingModuleName = null;
       device.isModuleActive = false;
+
+      setActiveDeployments((prevActiveDeployments) => {
+        return prevActiveDeployments.filter((dep) => dep.deviceId !== device.deviceId);;
+      });
     }
   };
 
@@ -153,8 +192,11 @@ const Demo = () => {
 
     const updatePromises = [];
 
+    const deployments = await fetchData("/file/manifest"); // Fetch all the deployments availble in orchestrator
+
     for (const log of logs) {
       const deviceId = getDeviceIdByName(log.deviceName);
+      const logReceivedTime = new Date(log.dateReceived).getTime(); // This is in milliseconds
 
       if (log.funcName === "thingi_health") {
         if (!deviceMap.has(log.deviceName)) {
@@ -172,11 +214,14 @@ const Demo = () => {
           device.deviceId = deviceId;
 
           // This will make sure any changes to deployment are updated in local storage
-          updatePromises.push(updateDeployment(deviceMap.get(log.deviceName)));
+          updatePromises.push(updateDeployment(deviceMap.get(log.deviceName), log.deviceName, deployments));
         }
-      } else if (log.funcName === "deployment_create") {
+        // Added log time and current time difference check to prevent to create multiple moving object for old logs when refreshing the page 
+      } else if (log.funcName === "deployment_create" && ((now - logReceivedTime) < 5000)) { 
         moveCodeAnimation(log.deviceName);
-        updatePromises.push(updateDeployment(deviceMap.get(log.deviceName)));
+        setTimeout(() => {
+          updatePromises.push(updateDeployment(deviceMap.get(log.deviceName), log.deviceName, deployments));
+        }, 5000);
       }
     }
 
@@ -272,30 +317,7 @@ const Demo = () => {
     };
   }, []);
 
-  // Added a timout to display the code move animation object as it gives wierd movement of (0,0) position to orchestrator position
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsCodeMoveObjsVisible(true);
-    }, 3000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  useEffect(() => {
-
-    // Moving object is referred to the animation of code moving interpretation
-    const setMovingObjInitialPosition = () => {
-      if (orchestratorRef.current) {
-        const orchestrator = orchestratorRef.current.getBoundingClientRect();
-        const orchestratorX = orchestrator.left + orchestrator.width / 2;
-        const orchestratorY = orchestrator.top + orchestrator.height / 2;
-
-        setCodeToFreezerObjPos({ x: orchestratorX, y: orchestratorY });
-        setCodeToWashingMachineObjPos({ x: orchestratorX, y: orchestratorY });
-      }
-    };
-
-    setMovingObjInitialPosition();
 
     // This logic will draw a line between the orchestrator and the equipment
     const connectOrchestratorToEquipment = (equipmentRef, equipmentName) => {
@@ -331,12 +353,10 @@ const Demo = () => {
     connectOrchestratorToEquipment(washingMachineRef, "washingMachine");
     window.addEventListener('resize', () => connectOrchestratorToEquipment(freezerRef, "freezer"));
     window.addEventListener('resize', () => connectOrchestratorToEquipment(washingMachineRef, "washingMachine"));
-    window.addEventListener('resize', setMovingObjInitialPosition);
 
     return () => {
       window.removeEventListener('resize', () => connectOrchestratorToEquipment(freezerRef, "freezer"));
       window.removeEventListener('resize', () => connectOrchestratorToEquipment(washingMachineRef, "washingMachine"));
-      window.removeEventListener('resize', setMovingObjInitialPosition);
     };
   }, []);
 
@@ -368,50 +388,64 @@ const Demo = () => {
         >
           <div id="orchestrator-freezer-line" />
           <div id="orchestrator-washingMachine-line" />
-          {isCodeMoveObjsVisible && (
+          {movingDeployments.map((deployment) => (
             <motion.div
-              className="round"
+              key={deployment.id}
               initial={{
-                x: codeToFreezerObjPos.x - 25,
-                y: codeToFreezerObjPos.y - 25,
-              }} // 25 pixels deducted to get the center of the object
+                x: deployment.startPos.x - 25,
+                y: deployment.startPos.y - 25,
+              }} // Center the animation object
               animate={{
-                x: codeToFreezerObjPos.x - 25,
-                y: codeToFreezerObjPos.y - 25,
+                x: deployment.endPos.x - 25,
+                y: deployment.endPos.y - 25,
               }}
               transition={{ type: "spring", duration: 5 }}
               style={{
                 position: "absolute",
-                width: "50px",
-                height: "50px",
-                backgroundColor: "#1E90FF",
-                borderRadius: "50%",
                 zIndex: 1,
               }}
-            />
-          )}
-          {isCodeMoveObjsVisible && (
+            >
+              <img
+                src={WebAssembly_Logo}
+                alt="Moving object"
+                style={{
+                  width: "50px",
+                  height: "50px",
+                }}
+              />
+            </motion.div>
+          ))}
+          {activeDeployments.map((deployment) => (
             <motion.div
-              className="round"
+              key={deployment.id}
               initial={{
-                x: codeToWashingMachineObjPos.x - 25,
-                y: codeToWashingMachineObjPos.y - 25,
-              }} // 25 pixels deducted to get the center of the object
+                x: deployment.wasmModuleIconPosition.x - 25,
+                y: deployment.wasmModuleIconPosition.y - 25,
+                width: "50px",  // Set initial width
+                height: "50px", // Set initial height
+              }} 
               animate={{
-                x: codeToWashingMachineObjPos.x - 25,
-                y: codeToWashingMachineObjPos.y - 25,
+                x: deployment.wasmModuleIconPosition.x - 25,
+                y: deployment.wasmModuleIconPosition.y - 25,
+                width: "20px",  // Animate to smaller width
+                height: "20px", // Animate to smaller height
               }}
               transition={{ type: "spring", duration: 5 }}
               style={{
                 position: "absolute",
-                width: "50px",
-                height: "50px",
-                backgroundColor: "#1E90FF",
-                borderRadius: "50%",
                 zIndex: 1,
               }}
-            />
-          )}
+            >
+              <img
+                src={WebAssembly_Logo}
+                alt="Moving object"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                }}
+              />
+            </motion.div>
+          ))}
           <Grid item xs={12} sm={3} minWidth={"77vh"}>
             <Box>
               <div
