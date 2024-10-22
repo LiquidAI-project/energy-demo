b6304a436635953195a6f325abade3b89aea4a6c
Lakshan Rathnayaka <lrlakshan@gmail.com>
2024-10-22 14:38:17 +0300

    Deploy energy quering wasm modules when pressing start button

--- run-demo/frontend/src/components/DemoControlls.jsx
+++ run-demo/frontend/src/components/DemoControlls.jsx
@@ -1,9 +1,32 @@
 import { Button } from "@mui/material";
+import { fetchData,fetchPostData } from "../services/apiService";
 
 const DemoControlls = () => {
+  const handleStart = async () => {
+    try {
+      const response = await fetchData("/file/manifest");
+
+      // Loop through all the responses
+      response.forEach((deploymentObj, index) => {
+        console.log(`Processing deployment ${index} with ID: ${deploymentObj._id}`);
+        wasmModuleDeployment(deploymentObj);
+    });
+    } catch (error) {
+      console.error("Error fetching manifests when starting the demo:", error);
+    }
+  };
+
+  const wasmModuleDeployment = async (deploymentObj) => {
+    try {
+        const response = await fetchPostData(`/file/manifest/${deploymentObj._id}`, JSON.stringify(deploymentObj));
+        console.log("Anthima", response);
+      } catch (error) {
+        console.error("Error when deploying wasm modules:", error);
+      }
+  };
   return (
     <div>
-      <Button variant="contained" color="primary" sx={{ mr: 4 }}>
+      <Button variant="contained" color="primary" sx={{ mr: 4 }} onClick={handleStart}>
         Start
       </Button>
       <Button variant="contained" color="primary">
