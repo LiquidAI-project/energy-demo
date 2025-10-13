640c12017ddeae8cce72c0951fb4606890eaaa55
Asma Jamil <asma.jamil@tuni.fi>
2025-10-13 13:20:48 +0300

    Socket url updated

--- run-demo/frontend/src/components/WebSocket.jsx
+++ run-demo/frontend/src/components/WebSocket.jsx
@@ -1,4 +1,4 @@
-const socket = new WebSocket("ws://orchestrator.tlt-cityiot.rd.tuni.fi/");
+const socket = new WebSocket("wss://orchestrator.tlt-cityiot.rd.tuni.fi/");
 
 socket.onopen = () => {
   console.log("✅ Connected to WebSocket server");
