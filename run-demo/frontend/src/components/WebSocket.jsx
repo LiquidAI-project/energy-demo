27f2a96e0f22f209fb570a0d802f01a8c1d3243c
Ville Heikkilä <ville.heikkila@tuni.fi>
2025-10-15 15:00:18 +0300

    Fix SOCKET_URL

--- run-demo/frontend/src/components/WebSocket.jsx
+++ run-demo/frontend/src/components/WebSocket.jsx
@@ -1,6 +1,6 @@
 const PUBLIC_HOST = import.meta.env.VITE_PUBLIC_HOST;
 const PUBLIC_PORT = import.meta.env.VITE_PUBLIC_PORT;
-const SOCKET_URL= `${PUBLIC_HOST}:${PUBLIC_PORT}`;
+const SOCKET_URL= `${PUBLIC_HOST.split("//")[1]}:${PUBLIC_PORT}`;
 
 const socket = new WebSocket(`wss://${SOCKET_URL}`);
 
