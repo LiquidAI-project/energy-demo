1f3819104233158fb07079c7eb026c482921aa06
Ville Heikkilä <ville.heikkila@tuni.fi>
2025-10-15 14:41:12 +0300

    Update websocket based on environment variables

--- run-demo/frontend/src/components/WebSocket.jsx
+++ run-demo/frontend/src/components/WebSocket.jsx
@@ -1,4 +1,8 @@
-const socket = new WebSocket("wss://130.230.52.191:4001");
+const PUBLIC_HOST = import.meta.env.VITE_PUBLIC_HOST;
+const PUBLIC_PORT = import.meta.env.VITE_PUBLIC_PORT;
+const SOCKET_URL= `${PUBLIC_HOST}:${PUBLIC_PORT}`;
+
+const socket = new WebSocket(`wss://${SOCKET_URL}`);
 
 socket.onopen = () => {
   console.log("✅ Connected to WebSocket server");
